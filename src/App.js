e6034f0f491936a242a9830e827bb1911cdae5b2
dfg1425@naver.com <dfg1425@naver.com>
2022-05-30 20:14:01 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+// import "./App.scss";
+import styles from "./App.module.css";
+import Button from "./components/Button";
+
+// style 안에 아래의 키값형태로 된 object가 들어가 있고,
+// 이 key값을 <div className={styles["App"]}> 와 같이 사용할 수 있다.
+
+// { 이름구성 : fileName_className__HashValue
+//   App: "App_App__B2Ebb"
+// App-header: "App_App-header__PyRwd"
+// App-link: "App_App-link__XPeK4"
+// App-logo: "App_App-logo__yMrik"
+// App-logo-spin: "App_App-logo-spin__1up6W"
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles["App"]}>
+      <header className={styles["header"]}>
+        <img src={logo} className={styles["logo"]} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Button</Button>
       </header>
     </div>
   );
---
+++ src/components/Button.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+class Button extends React.Component {
+  state = {
+    loading: false,
+  };
+
+  render() {
+    return (
+      <button
+        onClick={this.startLoading}
+        className={
+          this.state.loading
+            ? `${styles["button"]} + ${styles["loading"]}`
+            : styles["button"]
+        }
+        {...this.props}
+      />
+    );
+  }
+
+  startLoading = () => {
+    this.setState({
+      loading: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        loading: false,
+      });
+    }, 1000);
+  };
+}
+
+export default Button;
